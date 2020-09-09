8d10b995dae29b9bca8f589bf90a6a8a4c1cb7cd
Krocketeer <Krocketeer@github.com>
2020-09-08 23:34:56 -0700

    added project layout for Code section

--- src/pages/Code.js
+++ src/pages/Code.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import Airbnb from "../images/AirBnb Logo w Name.png";
 
-
 export default function Code() {
     const path = window.location.pathname
     const [activeProject, setActiveProject] = useState(path)
@@ -12,11 +11,11 @@ export default function Code() {
     return <div className="codeProjects" onClick={e => setActiveProject(e.key)}>
         <Link to="/code/project1">
             <MDBView hover>
-                <div id="project1" style={styles.projectBox} onClick={e => setActiveProject("/code/project1")}>
+                <div id={projectName[1]} style={styles.projectBox} onClick={e => setActiveProject("/code/project1")}>
                     <img style={styles.image} src={Airbnb} alt="airbnb logo" />
                     <MDBMask overlay="black-strong">
                         <div style={styles.hoverText}>
-                            <p>Project 1 title</p>
+                            <p>{projectName[1]} title</p>
                             <p>Project 1 Description</p>
                         </div>
                     </MDBMask>
@@ -26,25 +25,45 @@ export default function Code() {
 
         <Link to="/code/project2">
             <MDBView hover>
-                <div id="project2" style={styles.projectBox}>
-                    <p>Project 2</p>
-                    {/*<Link to="/code/codeProject2">{projectName[2]}</Link>*/}
+                <div id="project2" style={styles.projectBox} onClick={e => setActiveProject("/code/project2")}>
+                    <img style={styles.image} src={Airbnb} alt="airbnb logo" />
+                    <MDBMask overlay="black-strong">
+                        <div style={styles.hoverText}>
+                            <p>Project 2 title</p>
+                            <p>Project 2 Description</p>
+                        </div>
+                    </MDBMask>
                 </div>
-                <MDBMask className="flex-center" overlay="red-strong"></MDBMask>
             </MDBView>
         </Link>
 
         <Link to="/code/project3">
-            <div id="project3" style={styles.projectBox}>
-                <p>Project 3</p>
-                {/*<Link to="/code/codeProject3">{projectName[3]}</Link>*/}
-            </div>
+            <MDBView hover>
+                <div id="project3" style={styles.projectBox} onClick={e => setActiveProject("/code/project3")}>
+                    <img style={styles.image} src={Airbnb} alt="airbnb logo" />
+                    <MDBMask overlay="black-strong">
+                        <div style={styles.hoverText}>
+                            <p>Project 3 title</p>
+                            <p>Project 3 Description</p>
+                        </div>
+                    </MDBMask>
+                </div>
+            </MDBView>
         </Link>
 
-        <div id="project4" style={styles.projectBox}>
-            {/*<p>Project 4</p>*/}
-            <Link to="/code/project4">{projectName[4]}</Link>
-        </div>
+        <Link>
+            <MDBView hover>
+                <div id="project4" style={styles.projectBox} onClick={e => setActiveProject("/code/project4")}>
+                    <img style={styles.image} src={Airbnb} alt="airbnb logo" />
+                    <MDBMask overlay="black-strong">
+                        <div style={styles.hoverText}>
+                            <p>Project 4 title</p>
+                            <p>Project 4 Description</p>
+                        </div>
+                    </MDBMask>
+                </div>
+            </MDBView>
+        </Link>
     </div>
 }
 
@@ -76,10 +95,10 @@ const styles={
         fontSize: "24px"
     },
     image: {
-        minHeight: "200px",
+        minHeight: "198px",
         height: "20.5vw",
         maxHeight: "20.5vw",
-        minWidth: "450px",
+        minWidth: "448px",
         maxWidth: "47vw",
         width: "47vw",
     }
